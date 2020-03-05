7dac628591172d9c8382c118a880438a54641f5a
Leo <losoviz@gmail.com>
2020-03-05 17:23:40 +0800

    Load Composer's files

--- graphiql-block.php
+++ graphiql-block.php
@@ -10,5 +10,15 @@
  * @package         leoloso
  */
 
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+	exit;
+}
+define('GRAPHIQL_BLOCK_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GRAPHIQL_BLOCK_VERSION', '0.1');
+
+// Load Composer’s autoloader
+require_once (__DIR__.'/vendor/autoload.php');
+
 // Initialize this plugin
 (new \Leoloso\GraphiQLBlockWPPlugin\Plugin())->init();
